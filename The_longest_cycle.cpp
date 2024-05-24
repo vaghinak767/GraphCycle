3fefc93c9cf9e73b3d313047306009f8ec71e87b
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2024-05-24 09:13:23 +0400

    Update The_longest_cycle.cpp

--- The_longest_cycle.cpp
+++ The_longest_cycle.cpp
@@ -8,62 +8,70 @@ std::vector<int> path, longestCycle;
 int maxCycleLength = 0;
 int Nodes, Edges;
 
-void read(const std::string& filename) {
+void read(const std::string& filename) 
+{
     std::ifstream infile(filename);
-    if (!infile) {
+    if (!infile) 
+    {
         std::cerr << "Unable to open file";
         exit(1);
     }
-
     infile >> Nodes >> Edges;
     adj.resize(Nodes);
     visited.resize(Nodes);
-
     int u, v;
-    while (infile >> u >> v) {
+    while (infile >> u >> v) 
+    {
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
     infile.close();
 }
 
-void search(int start, int curr, int count) {
+void search(int start, int curr, int count) 
+{
     visited[curr] = true;
     path.push_back(curr);
-
-    for (int har : adj[curr]) {
-        if (!visited[har]) {
+    for (int har : adj[curr])
+    {
+        if (!visited[har])
+        {
             search(start, har, count + 1);
-        } else if (har == start && count > 2) {
-            if (count > maxCycleLength) {
-                maxCycleLength = count;
-                longestCycle = path;
-            }
-        }
+        } else if (har == start && count > 2) 
+               {
+                    if (count > maxCycleLength)
+                    {
+                        maxCycleLength = count;
+                        longestCycle = path;
+                    }
+               }
     }
-
     visited[curr] = false;
     path.pop_back();
 }
 
-int main() {
+int main() 
+{
     read("input.txt");
-
-    for (int i = 0; i < Nodes; i++) {
-        if (!adj[i].empty()) {
+    for (int i = 0; i < Nodes; i++) 
+    {
+        if (!adj[i].empty()) 
+        {
             fill(visited.begin(), visited.end(), false);
             path.clear();
             search(i, i, 1);
         }
+        if(maxCycleLenght == Edges)
+            break;
     }
-
     if (maxCycleLength > 0) {
         std::cout << "Maximum length simple cycle: ";
-        for (int node : longestCycle) {
+        for (int node : longestCycle) 
+        {
             std::cout << node << " ";
         }
-        std::cout << longestCycle[0] << std::endl;
-        std::cout << "Length: " << maxCycleLength << std::endl;
+        //std::cout << longestCycle[0] << std::endl;
+        std::cout << std::endl << "Length: " << maxCycleLength << std::endl;
     } else {
         std::cout << "No cycle found" << std::endl;
     }
